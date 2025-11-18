fb905d354fe64a666760a8ef127fa97461d3c4ec
Waleed <waleedghaleb@gmail.com>
2025-11-18 15:53:26 -0800

    this is rest of my project

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts =[
+    {
+        'author': 'Waleed',
+        'title':'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2025'
+    },
+    {
+            'author': 'Japreet',
+        'title':'Blog post 2',
+        'content': 'First post content',
+        'date_posted': 'August 29, 2025'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
