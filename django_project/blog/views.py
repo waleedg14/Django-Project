759868d6c9129d8a07b859eb1a4c39f324ab92e7
Waleed <waleedghaleb@gmail.com>
2025-11-20 16:15:34 -0800

    users posrtion

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Waleed',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2028'
-    },
-    {
-        'author': 'Ouiji',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2028'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': Posts.objects.all()
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
